0ec755b607142a672eefed64a6d4fb3c0724ce58
dendykurniawan <113506705+dendykurniawan@users.noreply.github.com>
2022-11-21 03:07:47 +0800

    Add files via upload

---
+++ Glide.java
@@ -0,0 +1,4 @@
+package com.dendykurniawanf55121088.githubuser;
+
+public class Glide {
+}
---
+++ User.java
@@ -0,0 +1,29 @@
+package com.dendykurniawanf55121088.githubuser;
+
+public class User {
+    private String photo;
+    private String name;
+    private String type;
+
+    public String getType() {
+        return type;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
